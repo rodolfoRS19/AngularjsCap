5b8b0673f0dd8a4ef92c0e222a091ffd68996a18
rrojasAlivebox <rrojas@alivebox.com>
2014-03-24 05:15:32 -0600

    add expression template

--- app/src/controllers/presentation/examplesModule.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Rodolfo on 20/03/14.
- */
--- app/src/controllers/presentation/presentationCtrl.js
+++ app/src/controllers/presentation/presentationCtrl.js
@@ -7,6 +7,11 @@ var presentationModule = angular.module('presentationModule',[])
 
         presentationModule.controller('presentationCtrl',function presentationCtrl($scope,$http){
 
+
+            $scope.templates = {
+                expressionUrl:  "../../app/src/templates/ejemplos/expression/expression.html"
+            };
+
             $http({
                 method:'GET',
                 url:'../../app/src/testData/basicsFeatures.json'
@@ -16,6 +21,38 @@ var presentationModule = angular.module('presentationModule',[])
                 return $scope.feature;
              })
 
+
+            $scope.onChangeExpressionContent = function(argIdContent){
+                $scope.ExpressionSwitchValue = null;
+                if(argIdContent == "index.html"){
+                    $scope.ExpressionSwitchValue = "index.html";
+                    $scope.loadExpressionIndex();
+                }
+            };
+
+            $scope.loadExpressionIndex = function(){
+                $scope.indexData = {
+                    line: null
+                }
+
+                $http({
+                    method:'GET',
+                    url:'../../app/src/testData/ExampleText/expression/index.txt'
+                })
+                    .success(function(response){
+                        $scope.indexData.line = $scope.splitTextLine(response);
+                        debugger;
+                        return $scope.indexData;
+                    })
+            }
+
+            $scope.splitTextLine = function(argText){
+                var lines;
+                for(var i = 0; i< argText.length; i++){
+                    lines = argText.split("\n");
+                }
+                return lines;
+            }
         });
 
         presentationModule.controller('expressionCtrl',function expressionCtrl($scope){
