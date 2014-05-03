315759daac92a151020f678357663b0fd6df3724
rrojasAlivebox <rrojas@alivebox.com>
2014-05-03 12:09:31 -0600

    new scopes

--- app/src/controllers/presentation/presentationCtrl.js
+++ app/src/controllers/presentation/presentationCtrl.js
@@ -20,34 +20,39 @@
         .controller('expressionCtrl',['$scope','$http','formatStringFactory',function($scope,$http,formatStringFactory){
 
             $scope.templates = {
-                expressionUrl:  "../../app/src/templates/ejemplos/expression/expression.html",
-                indexExample: "../../app/src/templates/ejemplos/expression/expressionExample.html"
+                expressionUrl:  "../../app/src/templates/ejemplos/expression/expression.html"
             };
 
-            $scope.loadExpressionIndex = function(){
-
-                $http({
-                    method:'GET',
-                    url:'../../app/src/testData/ExampleText/expression/index.txt'
-                })
-                    .success(function(response){
-                        $scope.indexData= formatStringFactory.doLinesText(response);
-                        return $scope.indexData;
-                    })
-            };
-
-          $scope.loadExpressionIndex();
+            $scope.imagesExample = {
+                templateUrl: '../../app/resources/images/expressionTemplate.png'
+            }
 
         }])
 
-        .controller('dataBindingCtrl',['$scope','$http','$window','$rootScope','formatStringFactory',
-          function($scope,$http,$window,$rootScope,formatStringFactory){
+        .controller('dataBindingCtrl',['$scope',function($scope){
 
           $scope.src = '../../app/resources/images/Two_Way_Data_Binding.jpg';
 
-          $scope.templates = {
-            dataBindingUrl:"../../app/src/templates/ejemplos/dataBinding/dataBinding.html",
-              indexExample:"../../app/src/templates/ejemplos/dataBinding/indexExample.html"
+          $scope.imagesExample = {
+             templateUrl: '../../app/resources/images/dataBindingTemplate.png'
           };
 
+          $scope.templates = {
+            dataBindingUrl:"../../app/src/templates/ejemplos/dataBinding/dataBinding.html"
+          };
+
+      }])
+
+        .controller('scopeCtrl',['$scope','$http',function($scope,$http){
+
+          $http({
+              method:'GET',
+              url:'../../app/src/testData/directivasNewScopes.json'
+          }).success(function(response){
+                 $scope.directiveList =  response.directives;
+              });
+
+          $scope.imagesExample = {
+              hirachieUrl:'../../app/resources/images/HirachieScope.png'
+          }
       }])
